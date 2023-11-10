4cfce35b2838c7ecd34565b1a70b3eec5245c85c
Vincent Smedinga <v.smedinga@amsterdam.nl>
2023-11-10 16:23:55 +0100

    Use regular link for logo until we have IconLink

--- src/components/shared/SiteHeader.tsx
+++ src/components/shared/SiteHeader.tsx
@@ -1,17 +1,17 @@
-import { Grid, Link, PageMenu } from '@amsterdam/design-system-react'
+import { Grid, PageMenu } from '@amsterdam/design-system-react'
 import { LoginIcon, MenuIcon, SearchIcon } from '@amsterdam/design-system-react-icons'
 
 export const SiteHeader = () => (
   <Grid style={{ paddingBlock: 'calc(var(--amsterdam-grid-gap) / 2)' }}>
     <Grid.Cell fullWidth>
       <div style={{ display: 'flex', justifyContent: 'space-between', gap: 'calc(var(--amsterdam-grid-gap ) * 4)' }}>
-        <Link href="#">
+        <a href="#">
           <img
             src="https://geocraft.nl/wp-content/uploads/2018/10/logo-gemeente-amsterdam-700x219.png"
             height="40"
             alt=""
           />
-        </Link>
+        </a>
         <PageMenu>
           <PageMenu.Link href="#" lang="en">
             English
