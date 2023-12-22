708a284f4ae1e0321514d581b9342ccc71e3b9f2
Vincent Smedinga <v.smedinga@amsterdam.nl>
2023-12-22 22:05:38 +0100

    Add link to skip to content

--- src/routes/amsterdam.nl/root.tsx
+++ src/routes/amsterdam.nl/root.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom'
-import { Grid, PageMenu, Screen } from '@amsterdam/design-system-react'
+import { Grid, PageMenu, Screen, SkipLink } from '@amsterdam/design-system-react'
 import { SiteHeader } from '../../components/amsterdam.nl/shared/SiteHeader'
 import { SiteFooter } from '../../components/amsterdam.nl/shared/SiteFooter'
 import PageMenuLink from '../../components/links/PageMenuLink'
@@ -7,24 +7,29 @@ import PageMenuLink from '../../components/links/PageMenuLink'
 export default function Root() {
   const baseUrl = import.meta.env.BASE_URL
   return (
-    <Screen maxWidth="wide">
-      <SiteHeader />
-      <Outlet />
-      <SiteFooter />
-      <Grid paddingVertical="small">
-        <Grid.Cell span="all">
-          <PageMenu>
-            <PageMenuLink to={baseUrl}>Home</PageMenuLink>
-            <PageMenuLink to={`${baseUrl}zoeken`}>Zoeken</PageMenuLink>
-            <PageMenuLink to={`${baseUrl}nieuws`}>Nieuws</PageMenuLink>
-            <PageMenuLink to={`${baseUrl}burgerzaken`}>Burgerzaken</PageMenuLink>
-            <PageMenuLink to={`${baseUrl}kunst-en-cultuur`}>Kunst en cultuur</PageMenuLink>
-            <PageMenuLink to={`${baseUrl}projecten`}>Projecten</PageMenuLink>
-            <PageMenuLink to={`${baseUrl}project`}>Project</PageMenuLink>
-            <PageMenuLink to={`${baseUrl}amopis`}>Amopis</PageMenuLink>
-          </PageMenu>
-        </Grid.Cell>
-      </Grid>
-    </Screen>
+    <>
+      <SkipLink href="#main">Direct naar inhoud</SkipLink>
+      <Screen maxWidth="wide">
+        <SiteHeader />
+        <main id="main">
+          <Outlet />
+        </main>
+        <SiteFooter />
+        <Grid paddingVertical="small">
+          <Grid.Cell span="all">
+            <PageMenu>
+              <PageMenuLink to={baseUrl}>Home</PageMenuLink>
+              <PageMenuLink to={`${baseUrl}zoeken`}>Zoeken</PageMenuLink>
+              <PageMenuLink to={`${baseUrl}nieuws`}>Nieuws</PageMenuLink>
+              <PageMenuLink to={`${baseUrl}burgerzaken`}>Burgerzaken</PageMenuLink>
+              <PageMenuLink to={`${baseUrl}kunst-en-cultuur`}>Kunst en cultuur</PageMenuLink>
+              <PageMenuLink to={`${baseUrl}projecten`}>Projecten</PageMenuLink>
+              <PageMenuLink to={`${baseUrl}project`}>Project</PageMenuLink>
+              <PageMenuLink to={`${baseUrl}amopis`}>Amopis</PageMenuLink>
+            </PageMenu>
+          </Grid.Cell>
+        </Grid>
+      </Screen>
+    </>
   )
 }
