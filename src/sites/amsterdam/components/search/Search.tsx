a58abf937b04a989e27abe70f1c80b992e008497
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-03-07 10:34:21 +0100

    Extract filter component and configuration

--- src/sites/amsterdam/components/search/Search.tsx
+++ src/sites/amsterdam/components/search/Search.tsx
@@ -1,42 +1,11 @@
-import { Breadcrumb, Card, Checkbox, Column, Grid, Heading, Paragraph, TextInput } from '@amsterdam/design-system-react'
+import { Breadcrumb, Card, Column, Grid, Heading, Paragraph, TextInput } from '@amsterdam/design-system-react'
 import { siteUrl } from '../../constants'
+import { Filters } from './Filters.tsx'
 
 export const Search = () => (
   <Grid paddingBottom="medium">
     <Grid.Cell span={{ narrow: 4, medium: 2, wide: 3 }}>
-      <div style={{ display: 'grid', gap: 'var(--amsterdam-grid-gap)' }}>
-        <div style={{ display: 'grid', gap: 'calc(var(--amsterdam-grid-gap) / 4)' }}>
-          <Heading level={2} size="level-4">
-            Contenttype
-          </Heading>
-          <Checkbox>Nieuws (10)</Checkbox>
-          <Checkbox>Regeling (10)</Checkbox>
-          <Checkbox>Werkzaamheden (5)</Checkbox>
-        </div>
-        <div style={{ display: 'grid', gap: 'calc(var(--amsterdam-grid-gap) / 4)' }}>
-          <Heading level={2} size="level-4">
-            Groep
-          </Heading>
-          <Checkbox>Bewoners (10)</Checkbox>
-          <Checkbox>Bedrijven (10)</Checkbox>
-          <Checkbox>Bezoekers (5)</Checkbox>
-        </div>
-        <div style={{ display: 'grid', gap: 'calc(var(--amsterdam-grid-gap) / 4)' }}>
-          <Heading level={2} size="level-4">
-            Soort afval
-          </Heading>
-          <Checkbox>Grofvuil (6)</Checkbox>
-          <Checkbox>Tuinafval (4)</Checkbox>
-          <Checkbox>Groente-, fruit-, etensresten en tuinafval (gfe/t) (3)</Checkbox>
-          <Checkbox>Textiel (3)</Checkbox>
-          <Checkbox>Huisvuil (2)</Checkbox>
-          <Checkbox>Papier en karton (2)</Checkbox>
-          <Checkbox>Glas (2)</Checkbox>
-          <Checkbox>Brood (1)</Checkbox>
-          <Checkbox>Asbest (1)</Checkbox>
-          <Checkbox>KCA (klein chemisch afval) (1)</Checkbox>
-        </div>
-      </div>
+      <Filters />
     </Grid.Cell>
     <Grid.Cell span={{ narrow: 4, medium: 5, wide: 8 }} start={{ narrow: 1, medium: 4, wide: 5 }}>
       <Breadcrumb>
