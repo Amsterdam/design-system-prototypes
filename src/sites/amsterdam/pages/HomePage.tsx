4dc7cdd8af72105220270304857b21838efbbfa5
Vincent Smedinga <v.smedinga@amsterdam.nl>
2024-03-07 10:37:57 +0100

    Add example of site-level alert

--- src/sites/amsterdam/pages/HomePage.tsx
+++ src/sites/amsterdam/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+import { CityOfficesClosed } from '../components/home/CityOfficesClosed.tsx'
 import { CurrentAffairs } from '../components/home/CurrentAffairs'
 import { Highlights } from '../components/home/Highlights'
 import { Search } from '../components/home/Search'
@@ -5,6 +6,7 @@ import { TopTasks } from '../components/home/TopTasks'
 
 export const HomePage = () => (
   <>
+    <CityOfficesClosed />
     <Search />
     <TopTasks />
     <Highlights />
