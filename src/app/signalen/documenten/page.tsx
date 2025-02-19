6ae8122841310ff63d96071060b16648feca915a
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-02-19 16:09:27 +0100

    Fix copy paste error in link url

--- src/app/signalen/documenten/page.tsx
+++ src/app/signalen/documenten/page.tsx
@@ -42,7 +42,7 @@ function Docs() {
   return (
     <Grid paddingVertical="medium">
       <Grid.Cell span={{ narrow: 4, medium: 6, wide: 7 }} start={{ narrow: 1, medium: 2, wide: 2 }}>
-        <BackLink className="ams-mb--xs" href="/signalen/contact-1">
+        <BackLink className="ams-mb--xs" href="/signalen/contact-2">
           Vorige vraag
         </BackLink>
         <Column className="ams-mb--md">
