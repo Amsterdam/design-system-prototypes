dddd767a422337b565557b93c697852ebdf0ef84
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-04-23 10:40:45 +0200

    Reset DOM reordering of headings

--- src/app/amopis/page.tsx
+++ src/app/amopis/page.tsx
@@ -25,14 +25,12 @@ function Project() {
           <Breadcrumb.Link href="#">Dashboard</Breadcrumb.Link>
           <Breadcrumb.Link href="#">Projecten</Breadcrumb.Link>
         </Breadcrumb>
-        <div className="amopis-visual-headings-order">
-          <Heading level={1} size="level-2">
-            Brug 423 opwaardering (Berlagebrug) – ams20192715
-          </Heading>
-          <Heading level={2} size="level-1">
-            Kerngegevens
-          </Heading>
-        </div>
+        <Heading level={2} size="level-1">
+          Kerngegevens
+        </Heading>
+        <Heading level={1} size="level-2">
+          Brug 423 opwaardering (Berlagebrug) – ams20192715
+        </Heading>
       </Grid.Cell>
       <Grid.Cell span={{ narrow: 4, medium: 8, wide: 10 }} start={{ narrow: 1, medium: 1, wide: 3 }}>
         <Tabs>
