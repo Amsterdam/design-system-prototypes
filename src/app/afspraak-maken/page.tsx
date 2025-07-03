5c00ee8b4705953f644e283c069284d347e9d516
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-07-03 15:12:35 +0200

    Implement introduction page

--- src/app/afspraak-maken/page.tsx
+++ src/app/afspraak-maken/page.tsx
@@ -1,13 +1,39 @@
 'use client'
 
-import { Grid, Heading, Paragraph } from '@amsterdam/design-system-react'
+import { Grid, Heading, OrderedList, Paragraph, StandaloneLink } from '@amsterdam/design-system-react'
+import { ChevronForwardIcon } from '@amsterdam/design-system-react-icons'
 
-export default function HomePage() {
+export default function Introductie() {
   return (
-    <Grid paddingBottom="x-large">
-      <Grid.Cell span="all">
-        <Heading level={1}>Afspraak maken</Heading>
-        <Paragraph>Hallo nieuw prototype</Paragraph>
+    <Grid paddingBottom="2x-large" paddingTop="large">
+      <Grid.Cell span={{ narrow: 4, medium: 6, wide: 6 }} start={{ narrow: 1, medium: 2, wide: 4 }}>
+        <Heading className="ams-mb-xl" level={1}>
+          Afspraak maken
+        </Heading>
+        <Heading className="ams-mb-s" level={2}>
+          Waar u dit formulier voor gebruikt
+        </Heading>
+        <Paragraph className="ams-mb-xl">
+          Met dit formulier maakt u een afspraak bij een Stadsloket in Amsterdam of Weesp.
+        </Paragraph>
+        <Heading className="ams-mb-s" level={2}>
+          De stappen in dit formulier
+        </Heading>
+        <OrderedList className="ams-mb-xl">
+          <OrderedList.Item>
+            <strong>Afspraak</strong> – Kies waarvoor u een afspraak wilt maken. Kies ook waar u de afspraak wilt
+            hebben. En wanneer.
+          </OrderedList.Item>
+          <OrderedList.Item>
+            <strong>Uw gegevens</strong> – Vul uw contactgegevens in.
+          </OrderedList.Item>
+          <OrderedList.Item>
+            <strong>Bevestiging</strong> – Controleer de gegevens die u heeft ingevuld. Verstuur de aanvraag.
+          </OrderedList.Item>
+        </OrderedList>
+        <StandaloneLink href="#" icon={ChevronForwardIcon}>
+          Start het formulier
+        </StandaloneLink>
       </Grid.Cell>
     </Grid>
   )
