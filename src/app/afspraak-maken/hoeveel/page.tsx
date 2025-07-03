2a0147098205ac5f35e47b5255cdff1bc93f7afe
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-07-03 16:54:25 +0200

    Add form step for ‘hoeveel’

--- src/app/afspraak-maken/hoeveel/page.tsx
+++ src/app/afspraak-maken/hoeveel/page.tsx
@@ -13,29 +13,27 @@ import {
 } from '@amsterdam/design-system-react'
 import { ChevronBackwardIcon, ChevronForwardIcon } from '@amsterdam/design-system-react-icons'
 
-const waaroverOptions = ['Paspoort aanvragen', 'Paspoor vermist of gestolen', 'Paspoort ophalen']
-
-export default function Waarover() {
+export default function Hoeveel() {
   return (
     <Grid paddingBottom="2x-large" paddingTop="large">
       <Grid.Cell span={{ narrow: 4, medium: 6, wide: 6 }} start={{ narrow: 1, medium: 2, wide: 4 }}>
         <Heading level={1}>Afspraak maken</Heading>
         <Paragraph className="ams-mb-xl">Stap 1 van 3: Afspraak</Paragraph>
-        <form action="/afspraak-maken/hoeveel" method="post">
+        <form>
           <Field className="ams-mb-xl">
-            <Label htmlFor="waarover">Kies waar het over gaat</Label>
-            <Select id="waarover">
-              <Select.Option value="">-- Kies een onderwerp --</Select.Option>
-              {waaroverOptions.map((label, index) => (
+            <Label htmlFor="hoeveel">Voor hoeveel personen wilt u een paspoort aanvragen?</Label>
+            <Select id="hoeveel">
+              <Select.Option value="">-- Kies aantal personen --</Select.Option>
+              {Array.from(Array(5).keys()).map((label, index) => (
                 <Select.Option key={index} value={index + 1}>
-                  {label}
+                  {label + 1}
                 </Select.Option>
               ))}
             </Select>
           </Field>
           <ActionGroup className="ams-mb-m">
             <Button
-              formAction="/afspraak-maken/waarvoor"
+              formAction="/afspraak-maken/waarover"
               icon={ChevronBackwardIcon}
               iconBefore
               type="submit"
