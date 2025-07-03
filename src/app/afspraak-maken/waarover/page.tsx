b543207c97801a95d7f03525871cc36d071a5aa6
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-07-03 16:46:34 +0200

    Add form step for ‘waarover’

--- src/app/afspraak-maken/waarover/page.tsx
+++ src/app/afspraak-maken/waarover/page.tsx
@@ -13,29 +13,20 @@ import {
 } from '@amsterdam/design-system-react'
 import { ChevronBackwardIcon, ChevronForwardIcon } from '@amsterdam/design-system-react-icons'
 
-const waarvoorOptions = [
-  'Paspoort',
-  'Identiteitskaart',
-  'Rijbewijs',
-  'Vluchtelingenpaspoort',
-  'Uw gegevens wijzigen',
-  'Verhuizen en adresgegevens',
-  'Parkeren',
-  'Sociaal Loket',
-]
+const waaroverOptions = ['Paspoort aanvragen', 'Paspoor vermist of gestolen', 'Paspoort ophalen']
 
-export default function Waarvoor() {
+export default function Waarover() {
   return (
     <Grid paddingBottom="2x-large" paddingTop="large">
       <Grid.Cell span={{ narrow: 4, medium: 6, wide: 6 }} start={{ narrow: 1, medium: 2, wide: 4 }}>
         <Heading level={1}>Afspraak maken</Heading>
         <Paragraph className="ams-mb-xl">Stap 1 van 3: Afspraak</Paragraph>
-        <form action="/afspraak-maken/waarover" method="post">
+        <form>
           <Field className="ams-mb-xl">
-            <Label htmlFor="waarvoor">Kies waar u voor wilt langskomen op het Stadsloket</Label>
-            <Select id="waarvoor">
+            <Label htmlFor="waarover">Kies waar het over gaat</Label>
+            <Select id="waarover">
               <Select.Option value="">-- Kies een onderwerp --</Select.Option>
-              {waarvoorOptions.map((label, index) => (
+              {waaroverOptions.map((label, index) => (
                 <Select.Option key={index} value={index + 1}>
                   {label}
                 </Select.Option>
@@ -43,9 +34,15 @@ export default function Waarvoor() {
             </Select>
           </Field>
           <ActionGroup className="ams-mb-m">
-            <StandaloneLink href="/afspraak-maken" icon={ChevronBackwardIcon}>
-              Terug naar de inleiding
-            </StandaloneLink>
+            <Button
+              formAction="/afspraak-maken/waarvoor"
+              icon={ChevronBackwardIcon}
+              iconBefore
+              type="submit"
+              variant="secondary"
+            >
+              Vorige vraag
+            </Button>
             <Button icon={ChevronForwardIcon} type="submit" variant="primary">
               Volgende vraag
             </Button>
