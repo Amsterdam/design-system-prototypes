ac54666411af3405e6e490532489d669cd917b65
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-07-03 17:03:14 +0200

    Add form step for ‘waar’

--- src/app/afspraak-maken/waar/page.tsx
+++ src/app/afspraak-maken/waar/page.tsx
@@ -3,6 +3,7 @@
 import {
   ActionGroup,
   Button,
+  DateInput,
   Field,
   Grid,
   Heading,
@@ -10,30 +11,41 @@ import {
   Paragraph,
   Select,
   StandaloneLink,
+  TimeInput,
 } from '@amsterdam/design-system-react'
 import { ChevronBackwardIcon, ChevronForwardIcon } from '@amsterdam/design-system-react-icons'
 
-export default function Hoeveel() {
+const waarOptions = ['Centrum', 'Nieuw-West', 'Noord', 'Oost', 'West', 'Zuid', 'Zuidoost', 'Weesp']
+
+export default function Waar() {
   return (
     <Grid paddingBottom="2x-large" paddingTop="large">
       <Grid.Cell span={{ narrow: 4, medium: 6, wide: 6 }} start={{ narrow: 1, medium: 2, wide: 4 }}>
         <Heading level={1}>Afspraak maken</Heading>
         <Paragraph className="ams-mb-xl">Stap 1 van 3: Afspraak</Paragraph>
-        <form action="/afspraak-maken/waar" method="post">
-          <Field className="ams-mb-xl">
-            <Label htmlFor="hoeveel">Voor hoeveel personen wilt u een paspoort aanvragen?</Label>
-            <Select id="hoeveel">
-              <Select.Option value="">-- Kies aantal personen --</Select.Option>
-              {Array.from(Array(5).keys()).map((label, index) => (
+        <form>
+          <Field className="ams-mb-l">
+            <Label htmlFor="waar">Stadsloket</Label>
+            <Select id="waar">
+              <Select.Option value="">-- Kies een stadsloket --</Select.Option>
+              {waarOptions.map((label, index) => (
                 <Select.Option key={index} value={index + 1}>
-                  {label + 1}
+                  {label}
                 </Select.Option>
               ))}
             </Select>
           </Field>
+          <Field className="ams-mb-l">
+            <Label htmlFor="datum">Datum</Label>
+            <DateInput id="datum" />
+          </Field>
+          <Field className="ams-mb-xl">
+            <Label htmlFor="tijd">Tijd</Label>
+            <TimeInput id="tijd" />
+          </Field>
           <ActionGroup className="ams-mb-m">
             <Button
-              formAction="/afspraak-maken/waarover"
+              formAction="/afspraak-maken/hoeveel"
               icon={ChevronBackwardIcon}
               iconBefore
               type="submit"
