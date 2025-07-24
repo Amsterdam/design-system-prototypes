66658c004f6cd12933516869a268e334b7f12abf
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-07-24 16:48:41 +0200

    poc: Add various examples of rich tables

---
+++ src/app/rijke-tabellen/layout.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import {
+  Grid,
+  Heading,
+  Logo,
+  Page,
+  PageFooter,
+  PageHeader,
+  Paragraph,
+  Row,
+  SkipLink,
+  Switch,
+} from '@amsterdam/design-system-react'
+import NextLink from 'next/link'
+import { PropsWithChildren, useState } from 'react'
+
+export default function RijkeTabellen({ children }: PropsWithChildren) {
+  const [compactMode, setCompactMode] = useState(false)
+
+  return (
+    <Page className={['ams-theme', compactMode ? 'ams-theme--compact' : undefined].join(' ')}>
+      <Grid>
+        <Grid.Cell span="all">
+          <SkipLink href="#main">Direct naar inhoud</SkipLink>
+        </Grid.Cell>
+      </Grid>
+      <PageHeader
+        logoLinkComponent={(props) => <NextLink {...props} href="/rijke-tabellen" />}
+        logoLinkTitle="Naar de homepage van Rijke tabellen"
+      />
+      <main id="main">{children}</main>
+      <PageFooter>
+        <Heading className="ams-visually-hidden" level={2}>
+          Over deze website
+        </Heading>
+        <PageFooter.Menu>
+          <NextLink href="/" legacyBehavior passHref>
+            <PageFooter.MenuLink>Prototypes</PageFooter.MenuLink>
+          </NextLink>
+          <li>
+            <Row gap="small">
+              <Paragraph size="small">Compact</Paragraph>
+              <Switch checked={compactMode} onChange={() => setCompactMode(!compactMode)} />
+            </Row>
+          </li>
+          {/* Append footer link here */}
+        </PageFooter.Menu>
+      </PageFooter>
+    </Page>
+  )
+}
---
+++ src/app/rijke-tabellen/link-in-cel/page.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+import { Breadcrumb, Grid, Heading, Link, Table } from '@amsterdam/design-system-react'
+import NextLink from 'next/link'
+import { useSearchParams } from 'next/navigation'
+import { ranking } from '../common'
+import { RankingTableHeaderRow } from '../common/RankingTableHeaderRow'
+
+export default function LinkInCel() {
+  const searchParams = useSearchParams()
+  const selection = Number(searchParams.get('selectie'))
+
+  return (
+    <Grid paddingBottom="x-large">
+      <Grid.Cell span="all">
+        <Breadcrumb>
+          <NextLink href="/rijke-tabellen" legacyBehavior passHref>
+            <Breadcrumb.Link>Rijke tabellen</Breadcrumb.Link>
+          </NextLink>
+        </Breadcrumb>
+        <Heading level={1} className="ams-mb-xl">
+          Eredivisie 2024/2025
+        </Heading>
+        <Table className="ams-mb-m">
+          <Table.Caption>
+            <Heading className="ams-mb-m" level={2}>
+              Eindstand
+            </Heading>
+          </Table.Caption>
+          <Table.Header>
+            <RankingTableHeaderRow />
+          </Table.Header>
+          <Table.Body>
+            {ranking.length ? (
+              ranking.map(
+                ({ drawn, goal_difference, goals_against, goals_for, lost, name, played, points, position, won }) => (
+                  <Table.Row key={position} className={selection === position ? 'ams-table__row--selected' : ''}>
+                    <Table.HeaderCell scope="row">{position}</Table.HeaderCell>
+                    <Table.Cell className="ams-table__cell--align-start ams-table__cell--nowrap">
+                      <NextLink href={`?selectie=${position}`} legacyBehavior passHref>
+                        <Link color={selection === position ? 'contrast' : undefined}>{name}</Link>
+                      </NextLink>
+                    </Table.Cell>
+                    <Table.Cell>{played}</Table.Cell>
+                    <Table.Cell>{won}</Table.Cell>
+                    <Table.Cell>{drawn}</Table.Cell>
+                    <Table.Cell>{lost}</Table.Cell>
+                    <Table.Cell>{points}</Table.Cell>
+                    <Table.Cell>{goals_for}</Table.Cell>
+                    <Table.Cell>{goals_against}</Table.Cell>
+                    <Table.Cell>{goal_difference}</Table.Cell>
+                  </Table.Row>
+                ),
+              )
+            ) : (
+              <Table.Row>
+                <Table.Cell colSpan={10}>Geen gegevens</Table.Cell>
+              </Table.Row>
+            )}
+          </Table.Body>
+        </Table>
+      </Grid.Cell>
+    </Grid>
+  )
+}
