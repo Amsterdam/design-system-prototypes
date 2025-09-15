74f8874abf75fe826b33258bfc86f1999366b7c4
Vincent Smedinga <v.smedinga@amsterdam.nl>
2025-09-15 17:28:29 +0200

    Wrap useSearchParams in a suspense boundary
    https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout

--- src/app/rijke-tabellen/link-in-cel/page.tsx
+++ src/app/rijke-tabellen/link-in-cel/page.tsx
@@ -3,6 +3,7 @@
 import { Breadcrumb, Grid, Heading, Link, Table } from '@amsterdam/design-system-react'
 import NextLink from 'next/link'
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 import { ranking } from '../common'
 import { RankingTableHeaderRow } from '../common/RankingTableHeaderRow'
 
@@ -31,32 +32,34 @@ export default function LinkInCel() {
             <RankingTableHeaderRow />
           </Table.Header>
           <Table.Body>
-            {ranking.length ? (
-              ranking.map(
-                ({ drawn, goal_difference, goals_against, goals_for, lost, name, played, points, position, won }) => (
-                  <Table.Row key={position} className={selection === position ? 'ams-table__row--selected' : ''}>
-                    <Table.HeaderCell scope="row">{position}</Table.HeaderCell>
-                    <Table.Cell className="ams-table__cell--align-start ams-table__cell--nowrap">
-                      <NextLink href={`?selectie=${position}`} legacyBehavior passHref>
-                        <Link color={selection === position ? 'contrast' : undefined}>{name}</Link>
-                      </NextLink>
-                    </Table.Cell>
-                    <Table.Cell>{played}</Table.Cell>
-                    <Table.Cell>{won}</Table.Cell>
-                    <Table.Cell>{drawn}</Table.Cell>
-                    <Table.Cell>{lost}</Table.Cell>
-                    <Table.Cell>{points}</Table.Cell>
-                    <Table.Cell>{goals_for}</Table.Cell>
-                    <Table.Cell>{goals_against}</Table.Cell>
-                    <Table.Cell>{goal_difference}</Table.Cell>
-                  </Table.Row>
-                ),
-              )
-            ) : (
-              <Table.Row>
-                <Table.Cell colSpan={10}>Geen gegevens</Table.Cell>
-              </Table.Row>
-            )}
+            <Suspense fallback={null}>
+              {ranking.length ? (
+                ranking.map(
+                  ({ drawn, goal_difference, goals_against, goals_for, lost, name, played, points, position, won }) => (
+                    <Table.Row key={position} className={selection === position ? 'ams-table__row--selected' : ''}>
+                      <Table.HeaderCell scope="row">{position}</Table.HeaderCell>
+                      <Table.Cell className="ams-table__cell--align-start ams-table__cell--nowrap">
+                        <NextLink href={`?selectie=${position}`} legacyBehavior passHref>
+                          <Link color={selection === position ? 'contrast' : undefined}>{name}</Link>
+                        </NextLink>
+                      </Table.Cell>
+                      <Table.Cell>{played}</Table.Cell>
+                      <Table.Cell>{won}</Table.Cell>
+                      <Table.Cell>{drawn}</Table.Cell>
+                      <Table.Cell>{lost}</Table.Cell>
+                      <Table.Cell>{points}</Table.Cell>
+                      <Table.Cell>{goals_for}</Table.Cell>
+                      <Table.Cell>{goals_against}</Table.Cell>
+                      <Table.Cell>{goal_difference}</Table.Cell>
+                    </Table.Row>
+                  ),
+                )
+              ) : (
+                <Table.Row>
+                  <Table.Cell colSpan={10}>Geen gegevens</Table.Cell>
+                </Table.Row>
+              )}
+            </Suspense>
           </Table.Body>
         </Table>
       </Grid.Cell>
