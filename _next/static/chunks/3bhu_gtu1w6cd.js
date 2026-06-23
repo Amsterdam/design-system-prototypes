ffa894a6c2b41e36a09677414acbfb51fefaded7
VincentSmedinga <v.smedinga@amsterdam.nl>
2026-06-23 22:20:46 +0000

    Deploying to gh-pages from @ Amsterdam/design-system-prototypes@05ae1fcb1b16e5ede47942362cd6ee5c1c8c58bb 🚀

---
+++ _next/static/chunks/3bhu_gtu1w6cd.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,24810,e=>{"use strict";var t=e.i(18566),u=e.i(71645);e.s(["default",0,function(){let e=(0,t.useRouter)();return(0,u.useEffect)(()=>{e.replace("/amopis/projecten")},[e]),null}])}]);
